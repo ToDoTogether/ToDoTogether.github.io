f8e9acb5613e78d79037cf7167d965cc79ee1f59
Miles Lenz <lenz.miles18@gmail.com>
2021-11-03 22:28:48 +0100

    added navigation bar in maxSubject and added basic style

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,18 @@
 function main() {
     hideElementsAtStart();
+    bindOnClickToButtons();
 }
 
 function hideElementsAtStart() {
     // Hide the maxSubject section
-    $("#sec_maxSubject").css("display", "none");
+    $("#sec_allSubjects").css("display", "none");  // ! working
+}
+
+function bindOnClickToButtons() {
+    // Bind function to the addNewSubject button
+    $("#btn_addNewSubject").on("click", function() {
+        console.log("new subject");
+    });
 }
 
 $(document).ready(main);
