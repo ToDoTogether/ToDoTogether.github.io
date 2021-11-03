51a8e08730c12054cdd9c83ed3239efe8bfba037
Miles Lenz <lenz.miles18@gmail.com>
2021-11-03 22:15:58 +0100

    added button with styling, js method to hide elements and main.css with variables

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,10 @@
 function main() {
-    
+    hideElementsAtStart();
+}
+
+function hideElementsAtStart() {
+    // Hide the maxSubject section
+    $("#sec_maxSubject").css("display", "none");
 }
 
 $(document).ready(main);
