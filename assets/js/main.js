af723206449294203ea2fbe60b714a53f5b0228e
Miles Lenz <lenz.miles18@gmail.com>
2021-11-03 22:45:44 +0100

    added toggleWindows() function

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,14 +5,27 @@ function main() {
 
 function hideElementsAtStart() {
     // Hide the maxSubject section
-    $("#allSubjects").css("display", "none");  // ! working
+    $("#maxSubject").css("display", "none");
 }
 
 function bindOnClickToButtons() {
-    // Bind function to the addNewSubject button
-    $("#addSubjectBtn").on("click", function() {
-        console.log("new subject");
+    // Bind function to the addNewSubject button in allSubjects and
+    // the goBack button in maxSubject
+    $("#addSubjectBtn, #goBackBtn").on("click", function() {
+        toggleWindows();
     });
+}
+
+function toggleWindows() {
+    // Change between allSubjects and maxSubject depending on
+    // which windows is displayed at the moment
+    if ($("#maxSubject").css("display") == "none") {
+        $("#maxSubject").css("display", "block");
+        $("#allSubjects").css("display", "none");
+    } else {
+        $("#maxSubject").css("display", "none");
+        $("#allSubjects").css("display", "block");
+    }
 }
 
 $(document).ready(main);
