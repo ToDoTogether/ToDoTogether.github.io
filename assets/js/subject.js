88d2fb549089caad3745c2497bf37ebdf21d0cff
Miles Lenz <lenz.miles18@gmail.com>
2021-11-04 21:16:31 +0100

    added dynamic style for maxSubject depending on what is checked

--- assets/js/subject.js
+++ assets/js/subject.js
@@ -17,24 +17,52 @@ function createSubject() {
 
     let h2 = document.createElement("h2");
     $(h2).text("Titel").attr("contenteditable", "true");
+    $(h2).on("keydown", function(e) {
+        if (e.keyCode == 13) {
+            e.preventDefault();
+        }
+    });
 
     let h4 = document.createElement("h4");
     $(h4).text("Erledigt: 0 | Ausstehend: 0");
 
     let hr = document.createElement("hr");
+    $(hr).addClass("hideHr");
 
     let divUnchecked = document.createElement("div");
     $(divUnchecked).addClass("unchecked");
     let divChecked = document.createElement("div");
     $(divChecked).addClass("checked");
 
+    let checkbox1 = createCheckbox(hr);
+    let checkbox2 = createCheckbox(hr);
+
+    // Append the elements in the correct order
+    $(divUnchecked).append(checkbox1, checkbox2);
+    $(div).append(h2, h4, divUnchecked, hr, divChecked);
+
+    return div;
+}
+
+function createCheckbox(hr) {
     let span = document.createElement("span");
 
     let input = document.createElement("input");
     $(input).attr({
         type: "checkbox",
         name: "item",
-    })
+    });
+    $(input).on("change", function() {
+        // Move the entry in the correct div
+        if (input.checked) {
+            $(span).appendTo($("#maxSubject .checked"));
+        } else {
+            $(span).appendTo($("#maxSubject .unchecked"));
+        }
+
+        // Check for styling updates
+        checkStyleOfEntries(hr);
+    });
 
     let label = document.createElement("label");
     $(label).attr("for", "item").text("Eintrag");
@@ -42,10 +70,21 @@ function createSubject() {
     let cross = document.createElement("span");
     $(cross).addClass("material-icons").text("close");
 
-    // Append the elements in the correct order
+    // Put the elements together
     $(span).append(input, label, cross);
-    $(divUnchecked).append(span);
-    $(div).append(h2, h4, divUnchecked, hr, divChecked);
 
-    return div;
+    return span;
+}
+
+function checkStyleOfEntries(hr) {
+    // Get amount of checkboxes
+    checkedCount = $("#maxSubject .checked").children("span").length;
+    uncheckedCount = $("#maxSubject .unchecked").children("span").length;
+
+    // Display the horizontal line only when at least one of both is checked
+    if (checkedCount == 0 || uncheckedCount == 0) {
+        $(hr).removeClass("showHr").addClass("hideHr");
+    } else {
+        $(hr).removeClass("hideHr").addClass("showHr");
+    }
 }
